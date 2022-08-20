ce1f2ca9fa3709e4d90b7059ac9fe5c5ef157c9c
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2022-08-20 18:12:33 -0300

    Concluindo projeto calculadora

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,37 @@
+public class Calculadora {
+    // Qual o problema? Criar uma calculadora
+    // Quais são os dados necessarios ? Dois valores para realizar as operações
+    // Quais operações necessarias? soma, subtração, divisão, multiplicação
+
+    public double soma(double valor1, double valor2){
+        return valor1+valor2;
+    }
+
+    public double subtracao(double valor1, double valor2){
+        return valor1-valor2;
+    }
+
+    public double multiplicacao(double valor1, double valor2){
+        return  valor1*valor2;
+    }
+
+    public double divisao(double valor1, double valor2){
+        return valor1 / valor2;
+    }
+
+    public double radiciacao(double qualValorQuerTirarRaiz, double indiceDaRaiz){
+        return (qualValorQuerTirarRaiz >= 1) ? Math.pow(qualValorQuerTirarRaiz, 1/indiceDaRaiz) : 0;
+    }
+
+    public double potenciacao(double qualValorQuerElevar, double expoente){
+        return (expoente >= 1) ? Math.pow(qualValorQuerElevar, expoente) : 0;
+    }
+
+    public double bhaskara(double a, double b, double c){
+        double delta = (Math.pow(b, 2)) - (4*a*c) ;
+        double x1 = (-b - Math.sqrt(delta)) / (2*a);
+        double x2 = (-b + Math.sqrt(delta)) / (2*a);
+        return x1+x2;
+    }
+
+}
---
+++ src/test/java/CalculadoraTeste.java
@@ -0,0 +1,84 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculadoraTeste {
+
+    @Test
+    public void deveSomarCorretamenteQuandoOsValoresForemInteiros(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(8,calculadora.soma(6,2),0);
+    }
+
+    @Test
+    public void deveSubtrairCorretamenteQuandoOsValoresForemInteiros(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(4,calculadora.subtracao(6,2),0);
+    }
+
+    @Test
+    public void deveMultiplicarCorretamenteQuandoNumerosForemInteiros(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(12,calculadora.multiplicacao(6,2),0);
+    }
+
+    @Test
+    public void deveDividirCorretamenteQuandoNumerosForemInteiros(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(3,calculadora.divisao(6,2),0);
+    }
+
+    @Test
+    public void deveSomarCorretamenteQuandoOsValoresForemFlutuantes(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(8.5,calculadora.soma(5.35,3.15),0);
+    }
+
+    @Test
+    public void deveSubtrairCorretamenteQuandoOsValoresForemFlutuantes(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(2.20,calculadora.subtracao(5.35,3.15),0.01);
+    }
+
+    @Test
+    public void deveMultiplicarCorretamenteQuandoNumerosForemFlutuantes(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(16.8525,calculadora.multiplicacao(5.35,3.15),0);
+    }
+
+    @Test
+    public void deveDividirCorretamenteQuandoNumerosPossuemPontosFlutuantes(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(1.7,calculadora.divisao(5.35,3.15),0.01);
+    }
+
+    @Test
+    public void deveTirarRaizDeIndiceNDeUmNumeroInteiroNãoNegativo(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(9,calculadora.radiciacao(531441,6),0.1);
+    }
+
+    @Test
+    public void deveElevarUmNumeroInteiroAUmExpoenteNãoNegativo(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(531441,calculadora.potenciacao(9,6),0);
+    }
+
+    @Test
+    public void deveTirarRaizDeIndiceNDeUmNumeroFlutuanteNãoNegativo(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(1.25,calculadora.radiciacao(3.0517578125,5),0);
+    }
+
+    @Test
+    public void deveElevarUmNumeroFlutuanteAUmExpoenteNãoNegativo(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(3.0517578125,calculadora.potenciacao(1.25,5),0);
+    }
+
+    @Test
+    public void deveFazerASomaDasRaizesDeUmaEquacaoQuadraticaQuandoPassadoSeusCoeficientes(){
+        Calculadora calculadora = new Calculadora();
+        Assert.assertEquals(1.5,calculadora.bhaskara(2,-3,1),0);
+    }
+
+}
